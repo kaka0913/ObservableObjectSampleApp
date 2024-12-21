8660e075a0e97c237b6e9c4f737932e480d76d72
kaka0913 <kabuyuu0913@gmail.com>
2024-12-21 11:48:32 +0900

    Initial Commit

---
+++ ObservableObjectTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableObjectTest
+//
+//  Created by 株丹優一郎 on 2024/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableObjectTest/ObservableObjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableObjectTestApp.swift
+//  ObservableObjectTest
+//
+//  Created by 株丹優一郎 on 2024/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ObservableObjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
